4a1c5a8e31f73d7bba3d6c6a4ea51972474af937
CharlotteImenkamp <56033548+CharlotteImenkamp@users.noreply.github.com>
2019-10-07 11:53:34 +0200

    VR Controll
    Walking in VR approved
    Tunnelling missing

--- Assets/My_Scripts/Movement.cs
+++ Assets/My_Scripts/Movement.cs
@@ -2,26 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
- public class Movement : MonoBehaviour
- {
-     public Vector2 trackpad;
-     public Vector3 moveDirection = new Vector3(0, 0, 0);
- 
+public class Movement : MonoBehaviour
+{
+    public Vector2 trackpad;
+    public Vector3 moveDirection = new Vector3(0, 0, 0);
+    public int speed = 1;
 
-     //public SteamVR_Input_Sources Hand;//Set Hand To Get Input From
-     //public float speed;
-     public GameObject Head;
+
+
+
+
+    //public SteamVR_Input_Sources Hand;//Set Hand To Get Input From
+    //public float speed;
+    public GameObject Head;
     // public CapsuleCollider Collider;
-     public GameObject AxisHand;//Hand Controller GameObject
-     public float Deadzone;//the Deadzone of the trackpad. used to prevent unwanted walking.
-     // Start is called before the first frame update
+    public GameObject AxisHand;//Hand Controller GameObject
+    public float Deadzone;//the Deadzone of the trackpad. used to prevent unwanted walking.
+                          // Start is called before the first frame update
 
 
-     private CapsuleCollider CapCollider;
-     public PhysicMaterial NoFrictionMaterial;
-     public SteamVR_Input_Sources MovementHand;//Set Hand To Get Input From
-     private Rigidbody rb;
-     private void Start()
+
+    private CapsuleCollider CapCollider;
+    public PhysicMaterial NoFrictionMaterial;
+    public SteamVR_Input_Sources MovementHand;//Set Hand To Get Input From
+    private Rigidbody rb;
+    private void Start()
     {
         CapCollider = GetComponent<CapsuleCollider>();
         rb = GetComponent<Rigidbody>();
@@ -31,43 +36,40 @@ using Valve.VR;
     void Update()
     {
         updateInput();
-        updateCollider(); //brauche ich das?
+        updateCollider();
+        //get the angle of the touch and correct it for the rotation of the controller 
+        moveDirection = Quaternion.AngleAxis(Angle(trackpad) + AxisHand.transform.localRotation.eulerAngles.y, Vector3.up) * Vector3.forward * speed;
 
-        //get the angle of the touch and correct it for the rotation of the controller
-        moveDirection = Quaternion.AngleAxis(Angle(trackpad) + AxisHand.transform.localRotation.eulerAngles.y, Vector3.up) * Vector3.forward;
-
-        Vector3 velocity = new Vector3(0, 0, 0); //??
-
-        if (trackpad.magnitude > Deadzone && trackpad.x+trackpad.y != 0)
+        rb.velocity = new Vector3(0, 0, 0);
+        if (trackpad.magnitude > Deadzone && trackpad.x + trackpad.y != 0)
         {//make sure the touch isn't in the deadzone and we aren't going to fast or zero
             CapCollider.material = NoFrictionMaterial;
             rb.velocity = moveDirection;
 
         }
-     }
-     public static float Angle(Vector2 p_vector2)
-     {
-         if (p_vector2.x < 0)
-         {
-             return 360 - (Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg * -1);
-         }
-         else
-         {
-             return Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg;
-         }
-     }
-
-
-     private void updateInput()
-     {
-         trackpad = TrackpadAction.GetAxis(MovementHand); ;
-     }
+    }
+    public static float Angle(Vector2 p_vector2)
+    {
+        if (p_vector2.x < 0)
+        {
+            return 360 - (Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg * -1);
+        }
+        else
+        {
+            return Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg;
+        }
+    }
 
     private void updateCollider()
     {
         CapCollider.height = Head.transform.localPosition.y;
         CapCollider.center = new Vector3(Head.transform.localPosition.x, Head.transform.localPosition.y / 2, Head.transform.localPosition.z);
     }
+
+    private void updateInput()
+    {
+        trackpad = SteamVR_Actions._default.MovementAxis.GetAxis(MovementHand);
+    }
 }
 
- 
+
--- Assets/My_Scripts/OddballCs.cs
+++ Assets/My_Scripts/OddballCs.cs
@@ -10,39 +10,39 @@ using System.Reflection;
 
 public class OddballCs : MonoBehaviour
 {
-    // Create the MATLAB instance 
-    MLApp.MLApp matlab = new MLApp.MLApp();
+    //    // Create the MATLAB instance 
+    //    //MLApp.MLApp matlab = new MLApp.MLApp();
 
-    private void Start()
-    {
+    //    private void Start()
+    //    {
 
-        Assembly myassembly = Assembly.LoadFrom("C:\Users\Charlotte\source\repos\ConsoleApp1\obj\Debug\Interop.MLApp.dll");
-        Type type = myassembly.GetType("Interop.MLApp");
+    //        //Assembly myassembly = Assembly.LoadFrom("C:\Users\Charlotte\source\repos\ConsoleApp1\obj\Debug\Interop.MLApp.dll");
+    //        Type type = myassembly.GetType("Interop.MLApp");
 
-        object instance = Activator.Createinstance(type);
+    //        //object instance = Activator.Createinstance(type);
 
-        MethodInfo[] methods = type.GetMethods();
-        //object res = methods[0].Invoke(instance, new object[] { 5, 3 });
+    //        MethodInfo[] methods = type.GetMethods();
+    //        //object res = methods[0].Invoke(instance, new object[] { 5, 3 });
 
-        //Console.WriteLine(res.ToString());
-        //Console.ReadLine();
-
-
-
-        // Change to the directory where the function is located 
-        matlab.Execute(@"cd C:\Users\Charlotte\Documents\GitHub\MazeVR\Assets\My_Scripts");
-
-        // Define the output 
-        // object result = null;
-
-        // Call the MATLAB function
-        matlab.Execute("OddballTest");
-    }
+    //        //Console.WriteLine(res.ToString());
+    //        //Console.ReadLine();
 
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    //        // Change to the directory where the function is located 
+    //        matlab.Execute(@"cd C:\Users\Charlotte\Documents\GitHub\MazeVR\Assets\My_Scripts");
+
+    //        // Define the output 
+    //        // object result = null;
+
+    //        // Call the MATLAB function
+    //        matlab.Execute("OddballTest");
+//}
+
+
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
+//    }
 }
