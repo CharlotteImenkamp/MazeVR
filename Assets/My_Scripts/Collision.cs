7562ae6e4aded54c38487eb1ea2d10daa2ea185a
CharlotteImenkamp <charlotte.imenkamp@gmail.com>
2019-10-16 17:26:09 +0200

    PathFinder fast mehrere kugeln

---
+++ Assets/My_Scripts/Collision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public GameObject collision;
+
+    private void Start()
+    {
+        //collision = GameObject.Find("collision");
+
+        //collision.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("GoldBall"))
+        {
+            other.gameObject.SetActive(false);
+            collision.SetActive(true);
+        }
+    }
+}
