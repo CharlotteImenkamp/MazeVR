baceee3b621b827bbc114ed63ffef81be070be15
CharlotteImenkamp <56033548+CharlotteImenkamp@users.noreply.github.com>
2019-10-30 17:56:15 +0100

    pointer works!
    todo: schieberegler
    movement check
    design
    counter
    prefabs

--- Assets/LaserInput.cs
+++ Assets/LaserInput.cs
@@ -1,47 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR;
 
 public class LaserInput : MonoBehaviour
 {
     public static GameObject currentObject;
-    int currentID; 
+    int currentID;
+    string currentName;
+    public SteamVR_Input_Sources m_TargetSouce;
+    public SteamVR_Action_Boolean m_ClickAction;
+
 
     void Start()
     {
         currentObject = null;
-        currentID = 0; 
+        currentID = 0;
+        currentName = ""; 
     }
-
 
     void Update()
     {
         RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0f); 
+        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0f);
 
-        for(int i= 0; i< hits.Length; i++)
+        //press
+        if (m_ClickAction.GetStateDown(m_TargetSouce))
         {
-            RaycastHit hit = hits[i];
-            print(hit);
-
-            int id = hit.collider.gameObject.GetInstanceID();
-
-            if(currentID != id)
+            for (int i = 0; i < hits.Length; i++)
             {
-                currentID = id;
-                currentObject = hit.collider.gameObject;
-
-                string name = currentObject.name; 
-                if(name == "Next")
+                if (hits[i].collider.gameObject.name == "StartButton")
                 {
-                    Debug.Log("Hit next"); 
-                }
-
-                string tag = currentObject.tag; 
-
-                if(tag == "Button")
-                {
-                    Debug.Log("HitButton"); 
+                    GameManager.Instance.StartLevel();
                 }
             }
         }
